7fa0d6dbae207b157cbab6680adc94621eca6cab
chatouillard <thibaut.chatellard@gmail.com>
2021-08-25 16:09:45 +0200

    uu

---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById("divulgacher").addEventListener("click", function(){
+    document.querySelector("")
+})
+
+let films = [
+{
+    "titre": "Les Évadés",
+    "spoil": "Ils s'évadent."
+},
+{
+    "titre": "L'Assassinat de Jesse James par le lâche Robert Ford",
+    "spoil": "Robert Ford va tuer Jesse James."
+},
+{
+    "titre": "There Will Be Blood",
+    "spoil": "Il va y'avoir du sang."
+},
+{
+    "titre": "Le Seigneur des Anneaux : Le Retour du roi",
+    "spoil": "Le roi va revenir."
+},
+{
+    "titre": "L'Homme qui tua Liberty Valance",
+    "spoil": "Libery Valance va se faire tuer."
+},
+{
+    "titre": "Il faut sauver le soldat Ryan",
+    "spoil": "Ils vont sauver Ryan."
+},
+{
+    "titre": "Le Fugitif",
+    "spoil": "Il va fuir."
+},
+{
+    "titre": "Le Discours d'un roi",
+    "spoil": "Un roi va faire un discours."
+},
+{
+    "titre": "Le Voyage dans la Lune",
+    "spoil": "Ils vont voyager dans la lune."
+},
+{
+    "titre": "Le Voleur de bicyclette",
+    "spoil": "Il va voler des bicyclettes."
+},
+];
+
+films[Math.floor(Math.random()*films.length)
